b4ab4c234dd911cebb3223c5a8fe7135f60fb348
Josh Castro <castrojoshua805@gmail.com>
2020-09-28 21:56:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,11 +3,10 @@ require 'pry'
 
 
 def start_game(player1, player2)
-  binding.pry 
-  "Hello #{player1} & #{playar2}"  
-  p "More code"
+  "Hello #{player1} & #{player2}"  
 end
 
 def play_game(player2)
+  binding.pry 
   "#{player1} is better than #{player2}"
 end
